14ee6e0f8b6f2fb7f86758df38609578ab7ffb56
Thiago Valentim <equipe@criawebs.com.br>
2012-04-14 21:56:18 -0300

    ajustando controle de request da url

--- tl/core/Request.class.php
+++ tl/core/Request.class.php
@@ -24,24 +24,25 @@ class Request {
 	}
 	
 	public function getAction() {
+		$this->setParam();
 		return (isset($this->uri[1])) ? $this->uri[1] : $this->registry->get('action');
 	}
 	
-	public function getParam() {
+	private function setParam() {
 		foreach ($this->uri as $key => $param) {
 			if($key > 1) {
 				array_push($this->param, $param);
 			}
 		}
-		return $this->param;
+		$this->registry->set('params', $this->param);
 	}
 	
 	private function dismemberUri() {
 		$url = parse_url($this->registry->get('url'));
 		$this->uri = str_replace($url['path'], "/", $this->uri);
 		$this->uri = substr_replace($this->uri, "", -strlen($this->uri), 1);
-		$this->uri = preg_replace("/^[?]\S+/", " ", $this->uri);
-		
+		$this->uri = preg_replace("/\/?[?]\S+/", "/index", $this->uri);
+
 		if(strrpos($this->uri, '/') == (strlen($this->uri) - 1)) {
 			$this->uri = substr_replace($this->uri, ' ', (strlen($this->uri) - 1), strlen($this->uri) );
 		}
@@ -53,11 +54,11 @@ class Request {
 	private function delegateControler() {
 		
 		if(isset($this->uri[0]) && preg_match("/{$this->registry->get('admin')}/", $this->uri[0])) {
-			$this->controler = "controler_backend_";
+			$this->registry->set('path', 'backend');
 		} else {
-			$this->controler = "controler_frontend_";
+			$this->registry->set('path', 'frontend');
 		}
-		
-		$this->controler .= (isset($this->uri[0]) && $this->uri[0] != '') ? $this->uri[0] : $this->registry->get('controller'); 	
+		$this->controler = $this->registry->get('path').'_controler_';
+		$this->controler .= (isset($this->uri[0]) && $this->uri[0] != '') ? $this->uri[0] : $this->registry->get('controller'); 
 	}
 }
